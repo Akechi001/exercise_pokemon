fc972786cb92a2b483ffe2beb7fd278d6753f3ba
Akechi001 <enricoarianthous@gmail.com>
2023-12-15 23:52:52 +0800

    done

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import pokemonsResultPage1 from "./__tests__/pokemon-result-limit-5-offset-0.json";
 
+
 const PokemonList = () => {
     // State to store the list of Pokemon
     const [pokemonList, setPokemonList] = useState([]);
@@ -36,28 +37,41 @@ const PokemonList = () => {
 
     // Move the return statement inside the functional component
     return (
-        <div>
-            <div>
-                <h2>Pokemon List</h2>
+        <div className="flex-col">
+            <div className="w-full mt-10 mb-10">
+                <h2 className="text-4xl font-bold mt-3 text-center">Pokemon List</h2>
             </div>
-            <div>
-                <ul>
-                    {pokemonList.map((pokemon) => (
-                        <li key={pokemon.name}>{pokemon.name}</li>
-                    ))}
-                </ul>
+
+            <div className="flex flex-row flex-wrap justify-center list-none"> {/* Updated here */}
+                {pokemonList && pokemonList.map((pokemon, i) => (
+                    <div key={i} className="w-60 h-80 border m-2 p-2 rounded-md text-center border border-black" key={pokemon.name}>
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${i+1}.png`} alt={pokemonList.name}  className="w-60 h-120"/>
+                        <p>#000{i+1}</p>
+                        <li className=" font-bold">{pokemon.name}</li>
+
+
+                    </div>
+                ))}
             </div>
-            <div>
-                <p>Displaying {pokemonList.length} of {jumlahPokemon} results</p>
+
+            <div className="w-full">
+                <p className="text-xl mt-3 text-center">
+                    Displaying {pokemonList.length} of {jumlahPokemon} results
+                </p>
             </div>
-            <div>
-                {pokemonList.length < jumlahPokemon && (
-                <button onClick={() => setOffset(offset+5)}>
-                    Load more
-                </button>
-                    )}
-            </div>
+
+            {pokemonList.length < jumlahPokemon && (
+                <div className="w-full mb-10 flex justify-center">
+                    <button
+                        className="bg-slate-200 w-28 h-10 rounded-xl font-bold mt-5"
+                        onClick={() => setOffset(offset + 5)}
+                    >
+                        Load more
+                    </button>
+                </div>
+            )}
         </div>
+
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.jsx'],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
